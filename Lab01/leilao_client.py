295d85bb85d1a942505e35bb0b8e2ad33955079b
JoaoVitorMintz <joaomintzz@gmail.com>
2026-08-13 17:46:11 -0300

    [UPDATE] Realização do exercicio 2 do laboratório 1, ainda necessário testar, mas construído o Leilão em tempo real!

--- Lab01/leilao_client.py
+++ Lab01/leilao_client.py
@@ -5,17 +5,26 @@ def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(("127.0.0.1", 5000))
 
+    # Recebe mensagem inicial
+    data = s.recv(4096).decode()
+
+    if not data:
+        print("ERRO: Mensagem inicial não recebida!")
+        s.close()
+        return
+
+    print(data, end="")
+
     while True:
+        resposta = input("Digite seu lance: ")
+        s.sendall((resposta + "\n").encode())
+
         data = s.recv(4096).decode()
         if not data:
             break
 
-        if data.endswith("> "):
-            resposta = input(data)
-            s.sendall((resposta + "\n").encode())
-        else:
-            print(data, end="")
-
+        print(data, end="")
+        
     s.close()
 
 
--- Lab01/leilao_server.py
+++ Lab01/leilao_server.py
@@ -5,88 +5,86 @@ import threading
 HOST = "0.0.0.0"
 PORT = 5000
 
-def fundo(preco, provento):
-    return {
-        'PRECO': preco,
-        'PROVENTO': provento,
-        'STATUS': preco + provento
-    }
+clientes_conectados = []
+maior_lance = 0.0
 
-# Com base no dia 08/12/2026
-FUNDOS = {
-    'BRCO11': fundo(114.12, 1.05),
-    'BTLG11': fundo(99.88, 0.81),
-    'HGLG11': fundo(147.76, 1.10),
-    'KNRI11': fundo(154.95, 1.38),
-    'MXRF11': fundo(9.60, 0.10),
-    'PVBI11': fundo(70.49, 0.40),
-    'XPCI11': fundo(83.69, 0.95),
-    'MCCI11': fundo(95.10, 1.00),
-    'KNCR11': fundo(106.50, 1.10),
-    'VISC11': fundo(105.42, 0.84)
-}
+def adicionar_cliente(conn):
+    clientes_conectados.append({ 
+        'CONN': conn, 
+        'LANCE': 0.0
+    })
 
-def comandos(conn):
-    # Este b já torna em bytes, não precisa do encode no final
-    conn.sendall(b"\n--- CONSULTA FINANCEIRA (MODO SEQUENCIAL) ---\n\n")
-    conn.sendall(b"Comandos: PRECO;TICKER | PROVENTO;TICKER | STATUS;TICKER\n")
-    conn.sendall(b"Digite SAIR para encerrar.\n\n")
+def alterar_lance(conn, lance):
+    for cliente in clientes_conectados:
+        if cliente['CONN'] == conn:
+            cliente['LANCE'] = lance
 
-    conn.sendall(b"> ")
+def broadcast(maior_atual):
+    for cliente in clientes_conectados:
+        cliente['CONN'].sendall(f"\nNOVO LANCE! Valor de {maior_atual} reais".encode())
+
+def valor_abaixo(conn, maior_atual):
+    conn.sendall(f"\nERRO: Valor abaixo do maior lance atual de {maior_atual} reais".encode())
+
+def rodar_leilao(conn):
+    global maior_lance # Função global para que possa ser alterada por todas as threads
+
+    conn.sendall(f"BEM VINDO AO LEILÃO!\nItem atual: Geladeira 2 portas!\n".encode())
+    
     while True:
-        data = conn.recv(1024).decode().strip()
-        if not data:
+
+        print(f"Maior lance atual de {maior_lance} reais.")
+
+        dados = conn.recv(1024)
+
+        if not dados:
             break
 
-        comando = data
+        lance = float(dados.decode())
+        lance_aceito = False
 
-        if comando == 'SAIR':
-            conn.sendall(b"\nPROCESSO FINALIZADO!\n")
-            break
+        # REGIÃO CRÍTICA:
+        if lance > maior_lance:
+            maior_lance = lance
+            print(f"Lance recebido: {lance} reais!") # Aviso ao servidor
+            lance_aceito = True
+            
 
-        partes = comando.split(";")
+        else:
+            print(f"O lance de {conn} foi abaixo do maior atual!") # Aviso ao servidor
+            valor_abaixo(conn, maior_lance) # Aviso apenas ao cliente que enviou o valor abaixo
 
-        if len(partes) != 2:
-            conn.sendall(b"\nERRO: Formato invalido. Use COMANDO;TICKER ou SAIR\n")
-            conn.sendall(b"> ")
-            continue
+        # FINAL DA REGIÃO CRÍTICA
+        if lance_aceito:
+            broadcast(maior_lance) # Aviso a todos os clientes
+            alterar_lance(conn, lance)
 
-        cmd, ticker = partes[0], partes[1]
 
-        if ticker not in FUNDOS.keys():
-            conn.sendall(b"\nERRO: FII nao encontrado\n")
-            conn.sendall(b"> ")
-            continue
+    conn.close()
 
-        if cmd not in FUNDOS[ticker].keys():
-            conn.sendall(b"\nERRO: Comando invalido\n")
-            conn.sendall(b"> ")
-            continue
-
-        valor = FUNDOS[ticker][cmd]
-        conn.sendall(f"{valor}\n".encode())
-        conn.sendall(b"> ")
 
 def main():
+    # INICIA O SOCKET
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((HOST, PORT))
-    conn, addr = s.accept()
-    t = threading.Thread(target=comandos, args=conn)
-    t.start()
-    t.daemon = True
-    
-    print(f"Leilão em Tempo Real ouvindo em {PORT}...")
-    print("Aviso: Permitido múltiplos usuários simultâneos")
+
+    s.listen(5)
 
     while True:
         conn, addr = s.accept()
-        print(f"Atendendo agora: {addr}")
+
+        # Cria thread para o cliente que acabou de entrar:
+        adicionar_cliente(conn)
+        print(f"Cliente conectado: {addr}")
+
+        # Configura para paralelizar o servidor para aceitar mais de um cliente
+        t = threading.Thread(target=rodar_leilao, args=(conn,))
+        t.daemon = True
+        t.start()   
         
-        comandos(conn) # O código "trava" aqui até a função terminar
-        
-        conn.close()
-        print(f"Cliente {addr} finalizado. Pronto para o próximo.\n")
+        print(f"Leilão em Tempo Real ouvindo em {PORT}...")
+        print("Aviso: Permitido múltiplos usuários simultâneos")
 
 if __name__ == "__main__":
     main()
